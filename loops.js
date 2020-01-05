195af9312cda55f4373371a3a9826077c1822dd6
Alumasa <alumasa.v@gmail.com>
2020-01-05 22:54:59 +0300

    Nested, do while and break loops.

--- loops.js
+++ loops.js
@@ -21,13 +21,48 @@ for (let i = 0; i < vacationSpots.length; i++ ){
 //Nested loops
 var bobsFollowers = ['Ann', 'Marie', 'Mix', 'Red'];
 var tinasFollowers = ['Mix', 'Marie', 'North'];
-var mutualFollowers = ['']
+var mutualFollowers = []
 
 for (let i = 0; i < bobsFollowers.length; i++) {
-  for (let j = 0; j < tinasFollowers.length; i++) {
-    if (bobsFollowers[i] == tinasFollowers[j]) {
-      mutualFollowers = tinasFollowers[j];
+  for (let j= 0; j < tinasFollowers.length; i++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(tinasFollowers[j]);
+      console.log(mutualFollowers);
     } 
   }
 }
+
+// a while loop
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+
+let currentCard;
+while (currentCard !== 'spade') {
+  currentCard = 
+  cards[Math.floor(Math.random() * 4)];
+  console.log(currentCard);
+}
+
+//do..while loop:
+var cupsOfSugarNeeded = 5;
+var cupsAdded = 0;
+do {
+  cupsAdded = cupsAdded + 1;
+  cupsAdded++;
+} while (cupsAdded < cupsOfSugarNeeded);
+
+//break
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.') {
+    break;
+  }
+}
+console.log('And if you don\'t know, now you know.')
+
+
+
 
