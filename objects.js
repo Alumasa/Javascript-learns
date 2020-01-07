1d08ef2592a00624f0d8d7703d537da330929ed6
Alumasa <alumasa.v@gmail.com>
2020-01-07 21:57:40 +0300

    Passing by reference and iteration through objects

--- objects.js
+++ objects.js
@@ -85,3 +85,56 @@ let spaceship = {
   let capFave = spaceship.crew['captain']['favorite foods'][0];
   spaceship.passengers = [{name: 'Space Dog'}]
   let firstPassenger = spaceship['passengers'][0];
+
+  //passed by reference - when passing an object through a function, the changes are permanent
+  let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth'
+  };
+  
+  let greenEnergy = obj => {
+    obj['Fuel Type'] = 'avocado oil';
+  }
+  
+  let remotelyDisable = obj => {
+    obj.disabled = true;
+  }
+  
+  greenEnergy(spaceship);
+  
+  remotelyDisable(spaceship);
+  
+  console.log(spaceship)
+
+  //looping through objects, iterating through objects using for..in
+  let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
